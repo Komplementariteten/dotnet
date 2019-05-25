1b4c877400cb1ec7baf81dc2fddad894244961ef
Oliver <os@ventose.cc>
2019-05-25 20:52:54 +0200

    gRpc Demo

---
+++ gRPCDemo/StatusServer.cs
@@ -0,0 +1,14 @@
+﻿using System.Threading.Tasks;
+using Grpc.Core;
+using Status;
+
+namespace gRPCDemo
+{
+    class StatusServer: Status.Status.StatusBase
+    {
+        public override Task<StatusResponse> GetStatus(StatusRequest request, ServerCallContext context)
+        {
+            return base.GetStatus(request, context);
+        }
+    }
+}
